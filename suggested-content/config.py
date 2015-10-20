ab5ab792eedf9b35ccbe680451961f1150e30b55
hmgibson23 <hugo.gibson@yahoo.co.uk>
2015-10-20 15:23:43 +0100

    Update the config with the correct buckets etc.

--- suggested-content/config.py
+++ suggested-content/config.py
@@ -1,1 +1,3 @@
-TRAINING_SET_BINARY="https://s3-eu-west-1.amazonaws.com/proposer-capi-snapshots/2015-10-15T10%3A44%3A08%2B00%3A00/capi-bump.bin"
+TRAINING_SET_BINARY="https://s3-eu-west-1.amazonaws.com/proposer-capi-snapshots/2015-10-15T10%3A44%3A08%2B00%3A00/capi-phrases.bin"
+TRAINING_SET_BINARY_BUCKET="proposer-capi-snapshots"
+TRAINING_SET_BINARY_KEY="2015-10-15T10:44:08+00:00/capi-phrases.bin"
