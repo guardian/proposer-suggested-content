7d47c126f1dd0c564ecbcc45bcc2e50f94476b3e
hmgibson23 <hugo.gibson@yahoo.co.uk>
2015-10-22 14:49:30 +0100

    Make the strip data actually strip the data

--- data-dump/strip-data.py
+++ data-dump/strip-data.py
@@ -1,33 +1,46 @@
-import re, argparse
+import re, argparse, string
 
 parser = argparse.ArgumentParser(description="Clean plaintext for training by word2vec")
 parser.add_argument('textfile', metavar='text-file', type=str, help="The file to strip")
 
 
 def prep_string(s):
-#    tmp = re.sub(ur"(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)", " " , s)
-    tmp = s
-    tmp.replace("'s", "")
-    tmp.replace("\"", "")
-    tmp.replace(".", " ")
-    tmp = re.sub(ur"[^\w\d\-\s]+",'',tmp)
-    return tmp
+    # remove twitter handles and usernames altogether as well as hashtags
+    # because no one cares about those
+    no_handles = re.sub(re.compile('(\s+|^)@\S+'), ' ', s)
+    no_twitter = re.sub(re.compile('(?:#[\w-]+\s*)+$'), ' ', no_handles)
+
+    # get rid of dashes that occur outside a {word}-{word} format
+    tmp = re.sub(re.compile('( |^)-+|-+( |$)'), ' ', no_twitter)
+
+    # singularise apostrophe jobs
+    next = tmp.replace("'s", "")
+
+    # strip all remaining punctuation apart from hypens - replace with a space to avoid
+    # badly punctuated words issues e.g. string,fun fun.string - punctuation should always
+    # be followed with a space
+    ret = re.sub('[%s]' % re.escape(string.punctuation.replace('-', '')), ' ', next)
+
+    # lowercase everything and return
+    return ret.lower()
+
 
 if __name__ == "__main__":
     args = vars(parser.parse_args())
 
-    input_file = args['text_file']
+    input_file = args['textfile']
     out_file = input_file + "-stripped"
 
 
     print("Stripping data from: " + str(input_file))
     print("Writing stripped data to: " + str(out_file))
 
-    segment = 1000000     # the amount of lines to write back at a time
+    segment = 10000000    # the amount of lines to write back at a time
     lines = []
     with open(input_file, "r") as r, open(out_file, "w") as w:
         for line in r:
             # do the processing and write the file
+            lines.append(prep_string(line))
             if len(lines) == segment:
                 w.writelines(lines)
                 lines = []
