85fe09d762242316b248c5eb40625aac7278c0b8
hmgibson23 <hugo.gibson@yahoo.co.uk>
2015-11-02 11:30:49 +0000

    Remove the unused line

--- suggested-content/helpers/ngrams.py
+++ suggested-content/helpers/ngrams.py
@@ -3,7 +3,6 @@ import helpers
 def bigrams(corpus):
     lowered = list(map(lambda x: x.lower(), corpus))
     grams = list_to_ngrams(" ".join(lowered).split(" "), 2)
-    #filtered = helpers.tfidf.filter_low_frequency_bigrams(lowered, grams)
     return map(lambda x: x[0] + '_' + x[1], grams)
 
 def list_to_ngrams(input_list, n):
